8419df3cb832cfab7b20cc0f0c5a95810e04270c
rtk5 <rithvik.matta@gmail.com>
2025-01-31 15:15:41 +0530

    fwerferfqw

--- MPCA/week 3/6.s
+++ MPCA/week 3/6.s
@@ -1,1 +1,17 @@
 .text
+    .global _start
+
+_start:
+    LDR R0, =0x788
+    MOV R1, #0
+
+SUM_LOOP:
+    AND R2, R0, #0xF
+    ADD R1, R1, R2
+    MOV R0, R0, LSR #4
+    CMP R0, #0
+    BNE SUM_LOOP
+
+    B .
+
+    .end
