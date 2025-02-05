9c25469f6b522330fac301a973e14d2f4aff1982
rtk5 <rithvik.matta@gmail.com>
2025-02-05 09:11:51 +0530

    OS_week5

--- OS/week 5/1)sender.c
+++ OS/week 5/1)sender.c
@@ -8,11 +8,14 @@ int main() {
     void *shared_memory;
     char buff[100];
     int shmid;
+
     shmid = shmget((key_t)1122,1024,0666|IPC_CREAT);
     printf("Key of shared memory is %d\n",shmid);   //unique identifier
+    
     shared_memory = shmat(shmid,NULL,0);    //attach
     printf("Process attached at %p\n",shared_memory);   //address
     printf("Enter some data to write to shared memory\n");  //write inside shared memory
+    
     read(0,buff,100);   
     strcpy(shared_memory,buff); //copying data to shared mem
     printf("Data read from shared memory is %s\n",(char*)shared_memory);
--- OS/week 5/2)reciever.c
+++ OS/week 5/2)reciever.c
@@ -8,8 +8,10 @@ int main() {
     void *shared_memory;
     char buff[100];
     int shmid;
+    
     shmid=shmget((key_t)1122,1024,0666);
     printf("Key of shared memory is %d\n",shmid);
+    
     shared_memory = shmat(shmid,NULL,0);
     printf("Process attached at %p\n",shared_memory);
     printf("Data read from shared memory is :%s\n",(char*)shared_memory);
