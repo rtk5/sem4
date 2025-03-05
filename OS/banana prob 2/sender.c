1b8884a06b5dc29618c9842ab1a315ac1c75f725
rtk5 <rithvik.matta@gmail.com>
2025-03-05 09:52:15 +0530

    mar 5

---
+++ OS/banana prob 2/sender.c
@@ -0,0 +1,43 @@
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+struct Student {
+    char name[50];
+    char SRN[20];
+    int sem;
+    float CGPA;
+};
+
+int main() {
+    void *shared_memory;
+    int shmid;
+
+    shmid = shmget((key_t)1122, sizeof(struct Student), 0666 | IPC_CREAT); 
+    printf("Key of shared memory is %d\n", shmid);
+
+    shared_memory = shmat(shmid, NULL, 0); //attatch
+    printf("Process attached at %p\n", shared_memory);
+
+    struct Student *student = (struct Student *)shared_memory;
+
+    // input
+    printf("Enter student name: ");
+    fgets(student->name, sizeof(student->name), stdin);
+    student->name[strcspn(student->name, "\n")] = 0;  // newline
+
+    printf("Enter SRN: ");
+    scanf("%s", &student->SRN);
+
+    printf("Enter semester: ");
+    scanf("%d", &student->sem);
+
+    printf("Enter CGPA: ");
+    scanf("%f", &student->CGPA);
+
+    printf("\nData written to shared memory.\n");
+
+    return 0;
+}
